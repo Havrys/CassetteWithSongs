61191395e7f310d7996e15b7cecfbb2356856132
Havrys <98223770+Havrys@users.noreply.github.com>
2022-01-26 11:22:45 +0200

    Update CASSETTE WITH SONGS.cpp

--- CASSETTE WITH SONGS.cpp
+++ CASSETTE WITH SONGS.cpp
@@ -1,3 +1,18 @@
+/*Description
+
+Suppose we have a set of n songs and want to store them on a tape. To find a song, we need to fast-forward all the songs before it.
+Let  A[1…n] be an array of songs’ durations. The time needed to access the k-th song is proportional to (A[1] + A[2] + ... + A[k-1]) .
+We say that the order of the songs on the tape is good if it minimizes the sum of times to access each song. Find the total length of 
+the first m songs on the tape with the good order of songs.
+
+Input
+ Quantity n of songs, a positive integer.
+ Number m of a song to find the cost accessing.
+ Four positive integers S, A, B, M. Using numbers S, A, B, M, the following pseudocode will generate the sequence of numbers.
+X[0] = S
+for i = 1 to n-1:
+ X[i] = (A * X[i-1] + B) mod M*/
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
